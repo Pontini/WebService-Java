1683ad9837072a4b27cef16bd8966ebbfbc4bd23
pontini <ewerton.pontini@gmail.com>
2016-07-26 17:15:19 -0300

    Passagem de parâmetro pela URL

--- Carros/src/br/com/livro/servlets/HelloServlets.java
+++ Carros/src/br/com/livro/servlets/HelloServlets.java
@@ -16,10 +16,18 @@ public class HelloServlets extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		resp.getWriter().print("Olá mundo Servlet");
+		resp.getWriter().print("Olá mundo Servlet\n");
 		
+		String nome = req.getParameter("nome");
+		String sobrenome = req.getParameter("sobrenome");
 		
+		resp.getWriter().print("Olá mundo "+nome+ " " +sobrenome);
 		
+		//Execute isto no navegador
+		//http://localhost:8080/Carros/hello?nome=Ewerton%20&sobrenome=Pontini
+	
+	
+	
 	}
 	
 
